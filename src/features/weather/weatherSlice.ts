fe1754b8e1a6ea210d5386b86825a1ffed3feb49
Sneax421 <curilov.german@web.de>
2025-04-11 11:06:58 +0200

    rtk

--- src/features/weather/weatherSlice.ts
+++ src/features/weather/weatherSlice.ts
@@ -1,11 +1,18 @@
 import {createSlice} from "@reduxjs/toolkit";
 import {fetchWeather} from "../api/asyncWeatherAction.ts";
+import {WeatherInfo} from "../../utils/types";
 
-
+const initialState: WeatherInfo = {
+    city: '',
+        country: '',
+        temp: 0,
+        pressure: 0,
+        sunset: 0
+};
 
 const weatherSlice = createSlice({
     name: 'weather',
-    initialState: {},
+    initialState,
     reducers: {},
     extraReducers: (builder) => {
         builder
